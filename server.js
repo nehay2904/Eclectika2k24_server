1f2e6ee45913ce60c48bcc955203e97663c03c41
Neha Yednurwar <yednurwarneha@gmail.com>
2023-06-15 16:40:30 +0530

    commit one

--- server.js
+++ server.js
@@ -2,15 +2,29 @@
 
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+
 const mongoose = require("mongoose")
 
-app.use(express.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes/auth.js
+const jwt = require('jsonwebtoken');
+
+const cors = require('cors')
+app.use(cors())
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
-// 
+
+const userModel = require('./Donor')
+const reqModel = require('./Request')
+
 // MONGODB_URI=mongodb+srv://neha2212:221200@cluster0.lhaoo6g.mongodb.net/test
 
 //connection of mongoose
@@ -22,6 +36,73 @@ mongoose.connect(MONGODB_URI, {
 }).catch((err) => {
   console.log(err)
 })
+
+app.get("/foo", async (req, res) => {
+  try {
+    const donor = await userModel.find({});
+    res.send(donor);
+    console.log(donor);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// Registration route
+app.post('/register', async (req, res) => {
+  try {
+    const {  donor_name, donor_email, password, blood_group, age, mobile_no, state, city} = req.body;
+
+    // Check if the user already exists
+    const existingUser = await userModel.findOne({ donor_email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+  
+    // Create a new user
+    const newUser = new userModel({
+      donor_name,
+      donor_email,
+      password,
+      blood_group,
+      age,
+      mobile_no,
+      state,
+      city
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+// Login route
+app.post('/login', async (req, res) => {
+  try {
+    const { donor_email, password } = req.body;
+
+    // Check if the user exists
+    const user = await userModel.findOne({ donor_email ,password});
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Create and sign a JWT token
+    const token = jwt.sign({ userId: user._id }, 'secretKey');
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 
 app.get('/', (req, res) => {
